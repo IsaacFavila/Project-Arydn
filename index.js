6a6e2a991d25911e3e3516fe75de1244d66b7739
Charles Ewing <charlesewing3@gmail.com>
2021-09-28 10:23:16 -0400

    initiate express server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+const morgan = require('morgan');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.get('/helloworld', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost${port}`)
+})
