3873caf4454d7f7b4e89557e70d9d0babb4d10fe
Isaac Favila <imfavila@gmail.com>
2021-09-29 18:33:57 -0500

    Finished formatting product information. Moving on to formatting product styles

--- products/models.js
+++ products/models.js
@@ -9,11 +9,26 @@ const getProducts = (page=1, count=5, callback) => {
 }
 
 const getInfo = (id, callback) => {
-  var queryStr = `select * from products where id = ${id}`;
-  // add features
+  var queryStr = `select * from products p, features f where p.id = ${id} and f.product_id = ${id}`;
   pool.query(queryStr, (err, results)  => {
-    console.log(results.rows);
-    callback(err, results.rows);
+    // Data formatting
+    var helperObj = results.rows[0];
+    helperObj.features = [];
+    // Push new feature to features
+    for (var i = 0; i < results.rows.length; i++) {
+      var newFeature = {
+        feature: results.rows[i].feature,
+        value: results.rows[i].value
+      };
+      helperObj.features.push(newFeature);
+    }
+    // More formatting to match API
+    helperObj.id = helperObj.product_id;
+    delete helperObj.product_id;
+    delete helperObj.feature;
+    delete helperObj.value;
+    console.log(helperObj);
+    callback(err, helperObj);
   });
 }
 
@@ -24,11 +39,15 @@ const getStyles = (id, callback) => {
     console.log(results.rows);
     callback(err, results.rows);
   });
+
 }
 const getRelated = (id, callback) => {
   var queryStr = `select related_product_id from related where current_product_id = ${id}`;
   pool.query(queryStr, (err, results)  => {
+    // Data formatting
     var helperArr =[];
+
+    // Push each object value to helperArr
     for (var i = 0; i < results.rows.length; i++) {
       helperArr.push(results.rows[i].related_product_id);
     }
