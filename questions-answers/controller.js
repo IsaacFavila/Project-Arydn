4e8e8c46a9dd7fcaa844198bec1d3aea4833a1c8
Charles Ewing <charlesewing3@gmail.com>
2021-09-29 19:17:02 -0400

    successfully getting Questions, but still need to include photos

--- questions-answers/controller.js
+++ questions-answers/controller.js
@@ -1,10 +1,58 @@
-
+const model = require('./model.js');
 
 // Get question list for product
 exports.getQuestions = function (req, res) {
-  var body = req.body;
-  console.log('BODY:', body)
-  res.send('getQuestions')
+  // model.getQuestions(req.body, (err, questionData) => {
+  //   if (err) {
+  //     console.log('QA Model Error:', err);
+  //   } else {
+  //     var questions = questionData.rows;
+
+  //     for (var i = 0; i < questions.length; i++) {
+  //       model.getAnswers(questions[i]['question_id'], (err, answerData) => {
+  //         if (err) {
+  //           console.log('getAnswers Model Error:', err);
+  //         } else {
+  //           questions[i].answers = answerData;
+  //         }
+  //       })
+  //     }
+
+  //     res.send({
+  //       product_id: req.body.product_id,
+  //       results: questions
+  //     })
+  //   }
+  // })
+  model.getQuestions(req.body)
+    .then(questionData => {
+      var questions = questionData.rows;
+      var promises = [];
+      for (var i = 0; i < questions.length; i++) {
+        promises.push(model.getAnswers(questions[i]['question_id']));
+      }
+      Promise.all(promises)
+        .then(answers => {
+          console.log('ANSWERS:', answers)
+          for (var i = 0; i < answers.length; i++) {
+            var answerObj = {};
+            for (var j = 0; j < answers[i].rows.length; j++) {
+              answerObj[answers[i].rows[j].id] = answers[i].rows[j];
+            }
+            questions[i].answers = answerObj;
+          }
+          res.send({
+            product_id: req.body.product_id,
+            results: questions
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 };
 
 // Get answers for question
--- questions-answers/model.js
+++ questions-answers/model.js
@@ -1,15 +1,31 @@
 const app = require('../index.js');
 const db = require('./index.js');
 
-exports.getQuestions = () => {
-  db.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    db.end()
-  })
+exports.getQuestions = ({product_id, page, count}) => {
+  page = page || 1;
+  var queryString = 'SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported FROM questions WHERE product_id = $1 LIMIT $2 OFFSET $3';
+  var queryParams = [product_id, count, page];
+  return db.query(queryString, queryParams);
+  // db.query(queryString, queryParams, (err, res) => {
+  //   if (err) {
+  //     callback(err);
+  //   } else {
+  //     callback(null, res);
+  //   }
+  // })
 };
 
-exports.getAnswers = () => {
-
+exports.getAnswers = (question_id) => {
+  var queryString = 'SELECT id, body, date, answerer_name, helpfulness FROM answers WHERE question_id = $1';
+  var queryParams = [question_id];
+  return db.query(queryString, queryParams);
+  // db.query(queryString, queryParams, (err, res) => {
+  //   if (err) {
+  //     callback(err);
+  //   } else {
+  //     callback(null, res);
+  //   }
+  // })
 };
 
 exports.addQuestion = () => {
