4e3927a975a1d82d2c29503218a4442e5046417c
Art Taphanand <arthur.taphanand@gmail.com>
2021-10-01 10:08:45 -0500

    update

--- reviews/models.js
+++ reviews/models.js
@@ -3,19 +3,18 @@ var db = require('./db_index.js');
 const getAllReviews = (product_id, count=5, page=1, callback) => {
   var q = `SELECT review_id, rating, summary, recommend, response, body, review_date, reviewer_name, helpfulness FROM reviews LIMIT ${page-1}, ${count}`;
   db.query(q, function(error, results) {
-    
     if (error) console.log('GetAll ERROR', error);
     console.log('RESULTS', results)
-    // callback(error, results);
+    callback(error, results);
   });
 };
 
 const getMetaReview = (id, callback) => {
   /// PRODUCT ID
-  // var q = `SELECT * FROM characteristics JOIN characteristic_reviews ON characteristics.id = characteristic_reviews.characteristic_id WHERE product_id=${id}`;
+  var q = `SELECT * FROM characteristics JOIN characteristic_reviews ON characteristics.id = characteristic_reviews.characteristic_id WHERE product_id=${id}`;
   db.query(q, function(error, results) {
     if (error) console.log('getMeta ERROR', error);
-    // console.log('RESULTS', results)
+    console.log('RESULTS', results)
     callback(error, results);
   });
 }
@@ -33,7 +32,7 @@ const postReview = (q, callback) => {
 
 const updateHelpful = (query, callback) => {
 
-  // var q = 
+  var q = ""
 
 };
 
