aa177cd8d0bda4fff155d1ee958466e1fdb0cf87
Charles Ewing <85757361+charlesewing3@users.noreply.github.com>
2021-10-04 16:49:37 -0400

    Update index.js
    Resolved merge conflicts (they were preventing me from deploying the app)

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
 const morgan = require('morgan');
 const mysql = require('mysql');
->>>>>>> main
 const app = express();
-const morgan = require('morgan');
-
 
 app.use(express.urlencoded({extended: true}));
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(morgan('dev'));
 
-<<<<<<< HEAD
+const productRouter = require('./products/routes.js');
+app.use('/db', productRouter);
+
+const qaRouter = require('./questions-answers/routes.js');
+app.use('/qa', qaRouter);
+
 const reviewsRouter = require('./reviews/routes.js');
 app.use('/', reviewsRouter);
   
-=======
 // const {reviewsRouter} = require('./reviews/routes.js');
 // app.use('/', reviewsRouter);
-
->>>>>>> main
-const productRouter = require('./products/routes.js');
-// app.use('/db', productRouter);
-
-<<<<<<< HEAD
-const qaRouter = require('./questions-answers/routes.js');
-=======
-// const qaRouter = require('./questions-answers/routes.js');
->>>>>>> main
-// app.use('/qa', qaRouter);
 
 app.get('/helloworld', function (req, res) {
   res.send('Hello world');
