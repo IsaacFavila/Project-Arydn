797f25d835382b387d7e74d6d34d8209a6aa845e
Isaac Favila <imfavila@gmail.com>
2021-10-06 16:04:04 -0500

    change index.js for product deployment

--- index.js
+++ index.js
@@ -11,21 +11,12 @@ app.use(morgan('dev'));
 const productRouter = require('./products/routes.js');
 app.use('/db', productRouter);
 
-const qaRouter = require('./questions-answers/routes.js');
-app.use('/qa', qaRouter);
-
-const reviewsRouter = require('./reviews/routes.js');
-app.use('/', reviewsRouter);
 
 // this is my loader.io authentication (charles)
-app.get('/loaderio-9d1abb859f618dc47e030676fcb4c41f.txt', function (req, res) {
-  res.send('loaderio-9d1abb859f618dc47e030676fcb4c41f');
+app.get('/', function (req, res) {
+  res.send('');
 })
 
-  
-app.get('/helloworld', function (req, res) {
-  res.send('Hello world');
-})
 
 
 module.exports = app;
